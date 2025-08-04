11811affaf1f5797bbe685b257893c182e3e51c6
react-dev9501 <rituraj@metaversitytechnologies.com>
2025-08-04 23:32:40 +0530

    Rule change

--- src/Common/News.tsx
+++ src/Common/News.tsx
@@ -1,11 +1,12 @@
 import Marquee from "react-fast-marquee";
 
 const News = () => {
+  const host = window.location.hostname;
   return (
     <Marquee
       style={{ minHeight: 28, fontSize: "14px" }}
       className="gx-fx-xl gx-bg-grey gx-text-white gx-text-uppercase gx-font-weight-semi-bold gx-border gx-bg-flex gx-align-items-center">
-      Welcome to antpro 🙏 antpro99.pro pe sabhi apni I'd khole
+      Welcome to 🙏 ${host}.
     </Marquee>
   );
 };
--- src/Common/Welcome.tsx
+++ src/Common/Welcome.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 const Welcome = ({ handleClose }: Props) => {
+  const host = window.location.hostname;
   return (
     <>
       <div
@@ -38,14 +39,14 @@ const Welcome = ({ handleClose }: Props) => {
               <h2>प्रिय ग्राहक,</h2>
               <p>
                 आपसे अनुरोध है हमारी कोई डुप्लीकेट साइट नही है हमारी आधिकारिक
-                साइट 'Antpro.in' से लॉगिन करें। लॉगइन करने से पहले साइट का नाम
-                जरूर देख लें। आपके समर्थन के लिए धन्यवाद। टीम antpro
+                साइट '{host?.toUpperCase()}' से लॉगिन करें। लॉगइन करने से पहले साइट का नाम जरूर
+                देख लें। आपके समर्थन के लिए धन्यवाद। टीम {host?.toUpperCase()}
               </p>
               <h2>Dear Client,</h2>
               <p>
                 We don't have any duplicate site , You are requested to login
-                with our official site Antpro.in. I only. Please check the site
-                name before you login. Thanks for your support. Team antpro
+                with our official site {host?.toUpperCase()}. I only. Please check the site
+                name before you login. Thanks for your support. Team {host?.toUpperCase()}
               </p>
             </div>
             <div className="modal-footer">
