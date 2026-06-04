daf86e0e108d17d4d02a1e50a5106814e403274b
vivek-metaversitytechnologies <vivek@metaversitytechnologies.com>
2026-06-04 22:59:24 +0530

    Score and TV url update

--- src/Pages/GameDetails/TvSection.tsx
+++ src/Pages/GameDetails/TvSection.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { useGetTvScoreDataQuery } from "../../store/service/userServices/userServices";
 
 interface Props {
   showFull: boolean;
@@ -7,62 +8,27 @@ interface Props {
 
 const TvSection = ({ showFull, showTv }: Props) => {
   const { id } = useParams();
-  const scoreUrl = id ? `https://admin.khelo7.com/socket-iframe-5/crickexpo/${id}` : "";
+
+  const { data: tvScoreData } = useGetTvScoreDataQuery({ matchId: id ?? "" });
 
   return (
     <>
       {showTv && (
         <iframe
           width="100%"
-          // ref={ref}
           className="tv-iframe"
           title="tv-live-match-container"
           height={"200px"}
-          style={{
-            marginBottom: "-9px",
-          }}
-          src={`${import.meta.env.VITE_TV_URL}/${id}`}
+          style={{ marginBottom: "-9px" }}
+          src={tvScoreData?.data?.tvUrl}
         />
-        // <>
-        //   {loadingTv ? (
-        //     <p style={{ color: "#fff", textAlign: "center" }}>
-        //       Loading stream...
-        //     </p>
-        //   ) : tvUrl ? (
-        //     typeof tvUrl === "string" && tvUrl.includes("<iframe") ? (
-        //       // Response is iframe HTML
-        //       <div
-        //         dangerouslySetInnerHTML={{ __html: tvUrl }}
-        //         style={{ width: "100%", height: "100%" }}
-        //       />
-        //     ) : (
-        //       // Response is just a URL
-        //       <iframe
-        //         src={tvUrl}
-        //         title="TV Stream"
-        //         style={{
-        //           width: "100%",
-        //           height: "100%",
-        //           border: "none",
-        //         }}
-        //         allowFullScreen
-        //       />
-        //     )
-        //   ) : (
-        //     <p style={{ color: "#fff", textAlign: "center" }}>
-        //       No stream available
-        //     </p>
-        //   )}
-        // </>
       )}
-      {scoreUrl ? (
-        <iframe
-          src={scoreUrl}
-          id="score_fs"
-          title="match-scorecard"
-          className={showFull ? "fs_match_size_full" : "fs_match_size"}
-        />
-      ) : null}
+      <iframe
+        src={tvScoreData?.data?.scoreUrl}
+        id="score_fs"
+        title="match-scorecard"
+        className={showFull ? "fs_match_size_full" : "fs_match_size"}
+      />
     </>
   );
 };
